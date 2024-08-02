ba74a6578509ee0c6cc9dfd8453a4e29f26af287
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-08-02 18:38:40 +0100

    modify log_box

--- components/logs.py
+++ components/logs.py
@@ -1,14 +1,15 @@
 import streamlit as st
-from ..options.enums import ConfigStateKeys
+from options.enums import ConfigStateKeys
 
 
-def log_box(value: str) -> str:
+def log_box() -> str:
     """Display a text area which shows that logs of the current pipeline run.
-
-    Args:
-        value (str): The text to show in the box.
 
     Returns:
         str: The text in the box.
     """
-    return st.text_area("Logs", value=value, disabled=True, key=ConfigStateKeys.LogBox)
+    return st.text_area(
+        "Logs",
+        # value=st.session_state.get(ConfigStateKeys.LogBox, ""),
+        key=ConfigStateKeys.LogBox,
+    )
