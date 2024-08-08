5268e4a61a3453904868f005e43dc98ff70a71cd
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-08-08 14:59:53 +0100

    add directory param

--- machine_learning/call_methods.py
+++ machine_learning/call_methods.py
@@ -4,25 +4,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-def plot_scatter(y, yp, r2, set_name, dependent_variable, model_name):
+
+def plot_scatter(y, yp, r2, set_name, dependent_variable, model_name, directory):
 
     # Plotting the dataset
     fig, ax = plt.subplots()
     ax.scatter(y, yp)
-    ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-    ax.set_xlabel('Measured ' + dependent_variable, fontsize = 13)
-    ax.set_ylabel('Predicted ' + dependent_variable, fontsize = 13)
+    ax.plot([y.min(), y.max()], [y.min(), y.max()], "k--", lw=4)
+    ax.set_xlabel("Measured " + dependent_variable, fontsize=13)
+    ax.set_ylabel("Predicted " + dependent_variable, fontsize=13)
     figure_title = "Prediction Error for" + model_name + " - " + set_name
-    plt.title(figure_title, fontsize = 13)
-    legend = 'R2: '+str(float("{0:.2f}".format(r2)))
-    plt.legend(['Best fit',legend],loc = 'upper left', fontsize = 13)
-    plt.grid(axis='both')
+    plt.title(figure_title, fontsize=13)
+    legend = "R2: " + str(float("{0:.2f}".format(r2)))
+    plt.legend(["Best fit", legend], loc="upper left", fontsize=13)
+    plt.grid(axis="both")
     plt.savefig(f"{directory}/{model_name}.png")
     plt.close()
 
 
 def save_actual_pred_plots(data, results, opt: argparse.Namespace, logger) -> None:
-
     """Save Actual vs Predicted plots for Regression models
     Args:
         data: Data object
@@ -42,7 +42,7 @@ def save_actual_pred_plots(data, results, opt: argparse.Namespace, logger) -> No
         y_test = [np.array(df) for df in data.y_test]
         y_train = [np.array(df) for df in data.y_train]
 
-        # Scatter plot of actual vs predicted values 
+        # Scatter plot of actual vs predicted values
         for model_name, model_options in opt.model_types.items():
             if model_options["use"]:
                 logger.info(f"Saving actual vs prediction plots of {model_name}...")
@@ -50,14 +50,28 @@ def save_actual_pred_plots(data, results, opt: argparse.Namespace, logger) -> No
                 for i in range(opt.n_bootstraps):
                     y_pred_test = results[i][model_name]["y_pred_test"]
                     y_pred_train = results[i][model_name]["y_pred_train"]
-                    dependent_variable = opt.dependent_variable  
+                    dependent_variable = opt.dependent_variable
 
                     # Plotting the training and test results
-                    plot_scatter(y_test[i], y_pred_test, results[i][model_name]["r2_test"], "Test", dependent_variable, model_name)
-                    plot_scatter(y_train[i], y_pred_train, results[i][model_name]["r2_train"], "Train", dependent_variable, model_name)  
-  
-                   # sns.scatterplot(
-                   #     x=y_test[i], y=y_pred_test, marker="o", s=30, color="black"
-                   # )
+                    plot_scatter(
+                        y_test[i],
+                        y_pred_test,
+                        results[i][model_name]["r2_test"],
+                        "Test",
+                        dependent_variable,
+                        model_name,
+                        directory,
+                    )
+                    plot_scatter(
+                        y_train[i],
+                        y_pred_train,
+                        results[i][model_name]["r2_train"],
+                        "Train",
+                        dependent_variable,
+                        model_name,
+                        directory,
+                    )
 
-
+                # sns.scatterplot(
+                #     x=y_test[i], y=y_pred_test, marker="o", s=30, color="black"
+                # )
