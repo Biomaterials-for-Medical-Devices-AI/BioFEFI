73e258842afa799b762227fd2da6a1e644ef319e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-08-08 15:38:02 +0100

    add metrics and rename plot file

--- machine_learning/call_methods.py
+++ machine_learning/call_methods.py
@@ -13,22 +13,25 @@ def plot_scatter(y, yp, r2, set_name, dependent_variable, model_name, directory)
     ax.plot([y.min(), y.max()], [y.min(), y.max()], "k--", lw=4)
     ax.set_xlabel("Measured " + dependent_variable, fontsize=13)
     ax.set_ylabel("Predicted " + dependent_variable, fontsize=13)
-    figure_title = "Prediction Error for" + model_name + " - " + set_name
+    figure_title = "Prediction Error for " + model_name + " - " + set_name
     plt.title(figure_title, fontsize=13)
-    legend = "R2: " + str(float("{0:.2f}".format(r2)))
+    legend = "R2: " + str(float("{0:.2f}".format(r2["value"])))
     plt.legend(["Best fit", legend], loc="upper left", fontsize=13)
     plt.grid(axis="both")
-    plt.savefig(f"{directory}/{model_name}.png")
+    plt.savefig(f"{directory}/{model_name}-{set_name}.png")
     plt.close()
 
 
-def save_actual_pred_plots(data, results, opt: argparse.Namespace, logger) -> None:
+def save_actual_pred_plots(
+    data, ml_results, opt: argparse.Namespace, logger, ml_metric_results
+) -> None:
     """Save Actual vs Predicted plots for Regression models
     Args:
         data: Data object
-        results: Results of the model
+        ml_results: Results of the model
         opt: Options
         logger: Logger
+        ml_metric_results: metrics of machine learning models
     Returns:
         None
     """
@@ -48,15 +51,15 @@ def save_actual_pred_plots(data, results, opt: argparse.Namespace, logger) -> No
                 logger.info(f"Saving actual vs prediction plots of {model_name}...")
 
                 for i in range(opt.n_bootstraps):
-                    y_pred_test = results[i][model_name]["y_pred_test"]
-                    y_pred_train = results[i][model_name]["y_pred_train"]
+                    y_pred_test = ml_results[i][model_name]["y_pred_test"]
+                    y_pred_train = ml_results[i][model_name]["y_pred_train"]
                     dependent_variable = opt.dependent_variable
 
                     # Plotting the training and test results
                     plot_scatter(
                         y_test[i],
                         y_pred_test,
-                        results[i][model_name]["r2_test"],
+                        ml_metric_results[model_name][i]["R2"]["test"],
                         "Test",
                         dependent_variable,
                         model_name,
@@ -65,7 +68,7 @@ def save_actual_pred_plots(data, results, opt: argparse.Namespace, logger) -> No
                     plot_scatter(
                         y_train[i],
                         y_pred_train,
-                        results[i][model_name]["r2_train"],
+                        ml_metric_results[model_name][i]["R2"]["train"],
                         "Train",
                         dependent_variable,
                         model_name,
