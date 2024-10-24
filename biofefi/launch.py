08b5bec4bbe3cf6a35a576a0f529b42f7d6ed6ba
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-10-24 15:49:08 +0100

    docstring for app entrypoint

--- biofefi/launch.py
+++ biofefi/launch.py
@@ -4,6 +4,10 @@ import logging
 
 
 def main():
+    """The entrypoint of BioFEFI.
+    This method shouldn't be called explicitly. Use the `biofefi` command in the
+    terminal after installing the app.
+    """
     app_path = os.path.join(os.path.dirname(__file__), "ui.py")
     try:
         subprocess.run(["streamlit", "run", app_path])
