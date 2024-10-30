14d5d4abdad127b99b2c6813a83906d8de581e29
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-10-30 12:31:45 +0000

    intro text draft

--- biofefi/main_page.py
+++ biofefi/main_page.py
@@ -7,3 +7,17 @@ st.set_page_config(
 )
 header_logo()
 sidebar_logo()
+
+st.write("# Welcome")
+st.write(
+    """
+    BioFEFI allows you rapidly develop machine learning models of many kinds, and evaluate their performance
+    down to a feature-by-feature level.
+    
+    You can create models to solve either **classification** problems (e.g. is this image a cat 🐱 or a dog 🐶?)
+    or **regression** problems (e.g. what will be the price of gold 🏅 tomorrow 📈?).
+
+    Your models can then be evaluated by general measures, such as accuracy, and by individual feature metrics,
+    such as SHAP.
+    """
+)
