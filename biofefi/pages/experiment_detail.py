d888d7581c56fc0616b85003113fa9edd65cfb6f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-10-31 15:55:20 +0000

    show results on expreiment detail page

--- biofefi/pages/experiment_detail.py
+++ biofefi/pages/experiment_detail.py
@@ -1,11 +1,21 @@
 import os
+from pathlib import Path
 import streamlit as st
 
 from biofefi.components.images.logos import sidebar_logo
+from biofefi.components.logs import log_box
 from biofefi.components.navigation import navbar
 from biofefi.components.experiments import experiment_selector
-from biofefi.options.enums import ViewExperimentKeys
-from biofefi.options.file_paths import biofefi_experiments_base_dir
+from biofefi.components.plots import plot_box
+from biofefi.options.enums import ConfigStateKeys, ViewExperimentKeys
+from biofefi.options.file_paths import (
+    biofefi_experiments_base_dir,
+    fi_plot_dir,
+    fuzzy_plot_dir,
+    log_dir,
+    ml_plot_dir,
+)
+from biofefi.services.logs import get_logs
 
 
 st.set_page_config(
@@ -17,6 +27,13 @@ navbar()
 header = st.session_state.get(ViewExperimentKeys.ExperimentName)
 
 st.header(header if header is not None else "View experiment")
+st.write(
+    """
+    On this page, you can select a completed experiment to view.
+
+    Select an experiment in the dropdown below to see your results.
+    """
+)
 
 # Get the base directory of all experiments
 base_dir = biofefi_experiments_base_dir()
@@ -25,4 +42,19 @@ choices = os.listdir(base_dir)
 choices = filter(lambda x: not x.startswith("."), choices)
 # Filter out files
 choices = filter(lambda x: os.path.isdir(os.path.join(base_dir, x)), choices)
+
 experiment_selector(choices)
+
+if experiment_name := st.session_state.get(ViewExperimentKeys.ExperimentName):
+    experiment_name = base_dir / experiment_name
+    st.session_state[ConfigStateKeys.LogBox] = get_logs(log_dir(experiment_name))
+    ml_plots = ml_plot_dir(experiment_name)
+    if ml_plots.exists():
+        plot_box(ml_plots, "Machine learning plots")
+    fi_plots = fi_plot_dir(experiment_name)
+    if fi_plots.exists():
+        plot_box(fi_plots, "Feature importance plots")
+    fuzzy_plots = fuzzy_plot_dir(experiment_name)
+    if fuzzy_plots.exists():
+        plot_box(fuzzy_plots, "Fuzzy plots")
+    log_box()
