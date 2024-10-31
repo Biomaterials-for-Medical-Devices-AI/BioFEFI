cbbb0b2798a2f2d47fab59da52afb89624279df5
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-10-31 10:08:24 +0000

    rename file

--- biofefi/pages/create_experiment.py
+++ biofefi/pages/create_experiment.py
@@ -2,6 +2,8 @@ import streamlit as st
 from biofefi.components.images.logos import sidebar_logo
 
 st.set_page_config(
-    page_title="BioFEFI",
+    page_title="New Experiment",
     page_icon=sidebar_logo(),
 )
+
+st.header("New experiment")
