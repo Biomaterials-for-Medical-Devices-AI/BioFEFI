eada9c4ac1d89e46516d635d9bd126f4c0beb539
Eduardo Aguilar <earguila00@hotmail.com>
2024-11-05 11:11:50 +0000

    Added model_selector function that allows to select which model to explain in the feature importance page

--- biofefi/components/experiments.py
+++ biofefi/components/experiments.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from pathlib import Path
-from biofefi.options.enums import ViewExperimentKeys
+from biofefi.options.enums import ViewExperimentKeys, ExplainModels
 from biofefi.options.file_paths import biofefi_experiments_base_dir
 
 
@@ -20,4 +20,23 @@ def experiment_selector(options: list) -> Path:
         index=None,
         placeholder="Experiment name",
         key=ViewExperimentKeys.ExperimentName,
+    )
+
+
+def model_selector(options: list) -> Path:
+    """Select
+
+    Args:
+        options (list): The list of model names to choose from.
+
+    Returns:
+        Path: The path to the experiment on disk.
+    """
+
+    return st.multiselect(
+        "Select an experiment",
+        options=options,
+        default=None,
+        placeholder="Models to explain",
+        key=ExplainModels.ExplainModels,
     )
