690471b739ecf43ec7d479b6982ebcff0fc146eb
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-11-05 10:48:16 +0000

    add options to PlottingOptions

--- biofefi/options/plotting.py
+++ biofefi/options/plotting.py
@@ -3,4 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class PlottingOptions:
-    pass
+    angle_rotate_yaxis_labels: int = 60
+    angle_rotate_xaxis_labels: int = 10
+    plot_axis_font_size: int = 8
+    plot_axis_tick_size: int = 8
+    plot_title_font_size: int = 20
+    plot_colour_scheme: str = "classic"
