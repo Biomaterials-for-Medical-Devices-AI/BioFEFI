60cd13f9c892f1d90f71a11b8b4b114d60042e2d
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-11-18 15:45:15 +0000

    tidy up execution options class

--- biofefi/options/execution.py
+++ biofefi/options/execution.py
@@ -5,12 +5,9 @@ from biofefi.options.enums import Normalisations, ProblemTypes
 
 @dataclass
 class ExecutionOptions:
-    data_path: str
+    data_path: str | None = None
     data_split: dict | None = None
     experiment_name: str = "test"
-    is_feature_engineering: bool = False
-    is_machine_learning: bool = False
-    is_feature_importance: bool = False
     random_state: int = 1221
     problem_type: ProblemTypes = ProblemTypes.Classification
     dependent_variable: str | None = None
