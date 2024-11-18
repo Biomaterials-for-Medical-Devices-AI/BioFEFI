cb759fd176fe1af49f1f606b647b485a4deb9fcf
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-11-18 14:14:59 +0000

    debug options

--- biofefi/options/ml.py
+++ biofefi/options/ml.py
@@ -4,9 +4,9 @@ from typing import Any
 
 @dataclass
 class MachineLearningOptions:
+    model_types: dict
     n_bootstraps: int = 3
     save_actual_pred_plots: bool = True
-    model_types: dict
     ml_log_dir: str = "ml"
     save_models: bool = True
     ml_plot_dir: str = "ml"
