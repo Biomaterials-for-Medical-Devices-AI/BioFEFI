c469afe31e36b86d1e6e6a1674fdd95d5c38d62e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-11-18 13:46:44 +0000

    remove mutable default from MachineLearningOptions

--- biofefi/options/ml.py
+++ biofefi/options/ml.py
@@ -6,29 +6,7 @@ from typing import Any
 class MachineLearningOptions:
     n_bootstraps: int = 3
     save_actual_pred_plots: bool = True
-    model_types: dict = {
-        "Linear Model": {"use": False, "params": {"fit_intercept": False}},
-        "Random Forest": {
-            "use": True,
-            "params": {
-                "n_estimators": 300,
-                "min_samples_split": 2,
-                "min_samples_leaf": 1,
-                "max_depth": 6,
-            },
-        },
-        "XGBoost": {
-            "use": True,
-            "params": {
-                "kwargs": {
-                    "n_estimators": 300,
-                    "max_depth": 6,
-                    "learning_rate": 0.01,
-                    "subsample": 0.5,
-                }
-            },
-        },
-    }
+    model_types: dict
     ml_log_dir: str = "ml"
     save_models: bool = True
     ml_plot_dir: str = "ml"
