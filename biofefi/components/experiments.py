601829c1d30884d90d923e64fab036fb3c81bac5
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-11-29 12:30:18 +0000

    fix experiments.py

--- biofefi/components/experiments.py
+++ biofefi/components/experiments.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import streamlit as st
 
 from biofefi.options.enums import ConfigStateKeys, ViewExperimentKeys
-from biofefi.options.file_paths import biofefi_experiments_base_dir
 
 
 def experiment_selector(options: list) -> str:
