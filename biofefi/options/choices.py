7a512ba962e198a8984f1c9fbde74510c36cce98
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-12-20 13:01:15 +0000

    move model problem choices

--- biofefi/options/choices.py
+++ biofefi/options/choices.py
@@ -1,5 +1,14 @@
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC, SVR
+from xgboost import XGBClassifier, XGBRegressor
+from biofefi.machine_learning.nn_models import (
+    BayesianRegularisedNNClassifier,
+    BayesianRegularisedNNRegressor,
+)
 from biofefi.options.enums import (
     DataSplitMethods,
+    ModelNames,
     Normalisations,
     ProblemTypes,
     SvmKernels,
@@ -26,3 +35,18 @@ DATA_SPLITS = [
     DataSplitMethods.Holdout.capitalize(),
     DataSplitMethods.KFold.capitalize(),
 ]
+MODEL_PROBLEM_CHOICES = {
+    (ModelNames.LinearModel, ProblemTypes.Classification): LogisticRegression,
+    (ModelNames.LinearModel, ProblemTypes.Regression): LinearRegression,
+    (ModelNames.RandomForest, ProblemTypes.Classification): RandomForestClassifier,
+    (ModelNames.RandomForest, ProblemTypes.Regression): RandomForestRegressor,
+    (ModelNames.XGBoost, ProblemTypes.Classification): XGBClassifier,
+    (ModelNames.XGBoost, ProblemTypes.Regression): XGBRegressor,
+    (ModelNames.SVM, ProblemTypes.Classification): SVC,
+    (ModelNames.SVM, ProblemTypes.Regression): SVR,
+    (
+        ModelNames.BRNNClassifier,
+        ProblemTypes.Classification,
+    ): BayesianRegularisedNNClassifier,
+    (ModelNames.BRNNRegressor, ProblemTypes.Regression): BayesianRegularisedNNRegressor,
+}
