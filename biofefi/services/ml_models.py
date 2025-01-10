bf26467779305a5d163217a61f2f428ef8ca183c
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-01-10 15:18:52 +0000

    Revert "remove unused function"
    This reverts commit 8f13c9741b27018e714420132f955d88c6293ef5.

--- biofefi/services/ml_models.py
+++ biofefi/services/ml_models.py
@@ -85,6 +85,42 @@ def load_models_to_explain(path: Path, model_names: list) -> dict[str, list]:
     return models
 
 
+def assert_model_param(model, model_params, logger: object = None) -> None:
+    """
+    Asserts that the model parameters are valid
+
+    Parameters
+    ----------
+    model: object
+        The model to check the parameters for
+    model_params: dict
+        The model parameters to check
+    logger: object
+        The logger to use for logging
+    """
+    if hasattr(model, "args"):
+        original_args = model.args
+    else:
+        original_args = list(inspect.signature(model).parameters)
+    args_to_remove = []
+    for arg in model_params:
+        if arg not in original_args:
+            logger.warning(
+                f"Model {model.__name__} does not have parameter {arg}, removing"
+            )
+            args_to_remove.append(arg)
+    for arg in args_to_remove:
+        model_params.pop(arg)
+    # check arguments for XGBoost
+    if "kwargs" in model_params:
+        for arguments, value in model_params["kwargs"].items():
+            model_params[arguments] = value
+        model_params.pop("kwargs")
+
+    logger.info(f"Using model {model.__name__} with parameters {model_params}")
+    return model_params
+
+
 def get_models(
     model_types: dict[str, dict],
     problem_type: str,
