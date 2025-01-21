2b96ad063f2af8bb39c311b033ddda97f53967c8
Eduardo Aguilar <earguila00@hotmail.com>
2025-01-21 09:48:05 +0000

    add enums for binary and multiclass classification

--- biofefi/options/enums.py
+++ biofefi/options/enums.py
@@ -81,6 +81,8 @@ class ProblemTypes(StrEnum):
     Auto = "auto"
     Classification = "classification"
     Regression = "regression"
+    Binary_Classification = "binary_classification"
+    Multi_Classification = "multi_classification"
 
 
 class SvmKernels(StrEnum):
