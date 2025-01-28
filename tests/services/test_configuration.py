99122c5b2e8144425978ad1e46f84f7f81c46a5f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-01-28 16:36:47 +0000

    test load execution options

--- tests/services/test_configuration.py
+++ tests/services/test_configuration.py
@@ -1,4 +1,7 @@
+import dataclasses
+import json
 from pathlib import Path
+from typing import Generator
 import pytest
 
 from biofefi.options.execution import ExecutionOptions
@@ -14,17 +17,29 @@ from biofefi.options.file_paths import (
 from biofefi.options.fuzzy import FuzzyOptions
 from biofefi.options.ml import MachineLearningOptions
 from biofefi.options.plotting import PlottingOptions
-from biofefi.services.configuration import save_options
+from biofefi.services.configuration import load_execution_options, save_options
 
 
 @pytest.fixture
-def execution_opts():
+def execution_opts() -> ExecutionOptions:
+    """Produce a test instance of `ExecutionOptions`.
+
+    Returns:
+        ExecutionOptions: The test instance.
+    """
     # Arrange
     return ExecutionOptions(data_path="test_data.csv", data_split=DATA_SPLITS[0])
 
 
 @pytest.fixture
-def execution_opts_file():
+def execution_opts_file_path() -> Generator[Path]:
+    """Produce the test `Path` to some execution options.
+
+    Delete the file if it has been created by a test.
+
+    Yields:
+        Generator[Path]: The `Path` to the execution options file.
+    """
     # Arrange
     experiment_path = Path.cwd()
     options_file = execution_options_path(experiment_path)
@@ -33,6 +48,30 @@ def execution_opts_file():
     # Cleanup
     if options_file.exists():
         options_file.unlink()
+
+
+@pytest.fixture
+def execution_opts_file(
+    execution_opts: ExecutionOptions, execution_opts_file_path: Generator[Path]
+) -> Path:
+    """Saves and `ExecutionOptions` object to a file given by `execution_opts_file_path`
+    and returns the `Path` to that file.
+
+    Cleanup is handled by the `execution_opts_file_path` fixture passed in the second
+    argument.
+
+    Args:
+        execution_opts (ExecutionOptions): Exexution options fixture.
+        execution_opts_file_path (Generator[Path]): File path fixture.
+
+    Returns:
+        Path: `execution_opts_file_path`
+    """
+    # Arrange
+    options_json = dataclasses.asdict(execution_opts)
+    with open(execution_opts_file_path, "w") as json_file:
+        json.dump(options_json, json_file, indent=4)
+    return execution_opts_file_path
 
 
 @pytest.fixture
@@ -122,12 +161,12 @@ def fuzzy_opts_file():
         options_file.unlink()
 
 
-def test_save_execution_opts(execution_opts, execution_opts_file):
+def test_save_execution_opts(execution_opts, execution_opts_file_path):
     # Act
-    save_options(execution_opts_file, execution_opts)
+    save_options(execution_opts_file_path, execution_opts)
 
     # Assert
-    assert execution_opts_file.exists()
+    assert execution_opts_file_path.exists()
 
 
 def test_save_plotting_opts(plotting_opts, plotting_opts_file):
@@ -160,3 +199,12 @@ def test_save_fuzzy_opts(fuzzy_opts, fuzzy_opts_file):
 
     # Assert
     assert fuzzy_opts_file.exists()
+
+
+def test_load_execution_options(execution_opts, execution_opts_file):
+    # Act
+    opts = load_execution_options(execution_opts_file)
+
+    # Assert
+    assert isinstance(opts, ExecutionOptions)
+    assert opts == execution_opts
