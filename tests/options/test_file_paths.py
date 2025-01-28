eb667638303cc5390cac4b3cb5370c6fc8089b4a
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-01-28 13:58:35 +0000

    test execution options path

--- tests/options/test_file_paths.py
+++ tests/options/test_file_paths.py
@@ -169,3 +169,16 @@ def test_plot_options_path():
     # Assert
     assert isinstance(actual_output, Path)
     assert actual_output == expected_output
+
+
+def test_execution_options_path():
+    # Arrange
+    experiment_path = fp.biofefi_experiments_base_dir() / "TestExperiment"
+    expected_output = experiment_path / "execution_options.json"
+
+    # Act
+    actual_output = fp.execution_options_path(experiment_path)
+
+    # Assert
+    assert isinstance(actual_output, Path)
+    assert actual_output == expected_output
