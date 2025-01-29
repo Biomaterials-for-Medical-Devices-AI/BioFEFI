812d583e761ef651212761539e541d91a7ca7d95
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-01-29 10:41:08 +0000

    test load fi opts

--- tests/services/test_configuration.py
+++ tests/services/test_configuration.py
@@ -1,5 +1,6 @@
 import dataclasses
 import json
+import logging
 from pathlib import Path
 from typing import Generator
 import pytest
@@ -19,6 +20,7 @@ from biofefi.options.ml import MachineLearningOptions
 from biofefi.options.plotting import PlottingOptions
 from biofefi.services.configuration import (
     load_execution_options,
+    load_fi_options,
     load_plot_options,
     save_options,
 )
@@ -163,7 +165,12 @@ def ml_opts_file():
 
 
 @pytest.fixture
-def fi_opts():
+def fi_opts() -> FeatureImportanceOptions:
+    """Produce a test instance of `FeatureImportanceOptions`.
+
+    Returns:
+        FeatureImportanceOptions: The test instance.
+    """
     # Arrange
     return FeatureImportanceOptions(
         global_importance_methods={},
@@ -173,7 +180,14 @@ def fi_opts():
 
 
 @pytest.fixture
-def fi_opts_file():
+def fi_opts_file_path() -> Generator[Path, None, None]:
+    """Produce the test `Path` to some FI options.
+
+    Delete the file if it has been created by a test.
+
+    Yields:
+        Generator[Path, None, None]: The `Path` to the FI options file.
+    """
     # Arrange
     experiment_path = Path.cwd()
     options_file = fi_options_path(experiment_path)
@@ -182,6 +196,30 @@ def fi_opts_file():
     # Cleanup
     if options_file.exists():
         options_file.unlink()
+
+
+@pytest.fixture
+def fi_opts_file(
+    fi_opts: FeatureImportanceOptions, fi_opts_file_path: Generator[Path, None, None]
+) -> Path:
+    """Saves and `FeatureImportanceOptions` object to a file given by `fi_opts_file_path`
+    and returns the `Path` to that file.
+
+    Cleanup is handled by the `fi_opts_file_path` fixture passed in the second
+    argument.
+
+    Args:
+        fi_opts (FeatureImportanceOptions): FI options fixture.
+        fi_opts_file_path (Generator[Path, None, None]): File path fixture.
+
+    Returns:
+        Path: `fi_opts_file_path`
+    """
+    # Arrange
+    options_json = dataclasses.asdict(fi_opts)
+    with open(fi_opts_file_path, "w") as json_file:
+        json.dump(options_json, json_file, indent=4)
+    return fi_opts_file_path
 
 
 @pytest.fixture
@@ -202,9 +240,9 @@ def fuzzy_opts_file():
         options_file.unlink()
 
 
-def test_save_execution_opts(plotting_opts, execution_opts_file_path):
+def test_save_execution_opts(execution_opts, execution_opts_file_path):
     # Act
-    save_options(execution_opts_file_path, plotting_opts)
+    save_options(execution_opts_file_path, execution_opts)
 
     # Assert
     assert execution_opts_file_path.exists()
@@ -226,12 +264,12 @@ def test_save_ml_opts(ml_opts, ml_opts_file):
     assert ml_opts_file.exists()
 
 
-def test_save_fi_opts(fi_opts, fi_opts_file):
+def test_save_fi_opts(fi_opts, fi_opts_file_path):
     # Act
-    save_options(fi_opts_file, fi_opts)
+    save_options(fi_opts_file_path, fi_opts)
 
     # Assert
-    assert fi_opts_file.exists()
+    assert fi_opts_file_path.exists()
 
 
 def test_save_fuzzy_opts(fuzzy_opts, fuzzy_opts_file):
@@ -258,3 +296,17 @@ def test_load_plot_options(plotting_opts, plotting_opts_file):
     # Assert
     assert isinstance(opts, PlottingOptions)
     assert opts == plotting_opts
+
+
+def test_load_fi_options(fi_opts, fi_opts_file):
+    # Arrange
+    path_to_non_existent = Path("non_existent.json")  # test the `None` case
+
+    # Act
+    opts = load_fi_options(fi_opts_file)
+    non_existent_opts = load_fi_options(path_to_non_existent)
+
+    # Assert
+    assert isinstance(opts, FeatureImportanceOptions)
+    assert opts == fi_opts
+    assert non_existent_opts is None
