467d135020553d36fc83c945029af47f3ecced44
Eduardo Aguilar <earguila00@hotmail.com>
2025-02-04 11:28:30 +0000

    add keys for parameters of variable selection

--- biofefi/options/preprocessing.py
+++ biofefi/options/preprocessing.py
@@ -4,5 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class PreprocessingOptions:
     feature_selection_methods: dict
+    variance_threshold: float
+    correlation_threshold: float
+    lasso_regularisation_term: float
     independent_variable_normalisation: str = "none"
     dependent_variable_transformation: str = "none"
