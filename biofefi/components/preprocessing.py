8b9df498e637685aeb7776c5336fa35f9ecb92bf
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-02-11 13:17:21 +0000

    original view component

--- biofefi/components/preprocessing.py
+++ biofefi/components/preprocessing.py
@@ -13,3 +13,16 @@ def preprocessed_view(data: pd.DataFrame):
     st.write(data)
     st.write("### Processed Data Description")
     st.write(data.describe())
+
+
+@st.experimental_fragment
+def original_view(data: pd.DataFrame):
+    """Display the original data to the user.
+
+    Args:
+        data (pd.DataFrame): The original data to show.
+    """
+    st.write("### Original Data")
+    st.write(data)
+    st.write("### Original Data Description")
+    st.write(data.describe())
